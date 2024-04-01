b344ee23aff41fea5d577934a27190f267670184
Lucas Vignau <lucas.vignau@etu.umontpellier.fr>
2024-04-01 19:54:07 +0200

    cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const init = require('./init');
 const allowedOrigins = [
   'http://localhost:3000',
   'https://festival-jeu-mtp.onrender.com',
+  'https://fdjeuxfront.onrender.com',
 ];
 
 app.use(cors({
